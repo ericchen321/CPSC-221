d2b8fe57c630c5a399bdaa506e2072396bf26b50
eric <cgx1997@outlook.com>
2019-02-04 11:33:40 -0800

    added some mem leak exercises

--- practice/default-cnstr/main.cpp
+++ practice/default-cnstr/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -12,4 +13,9 @@ public:
 
 int main(){
   A objA;
+  int *ptr = new int(3); // check if memory leak happens without explictly delete
+                         // result: yes for primary types without explict deletetion, will have mem leak
+  delete ptr;
+  vector<int> v;
+  v.push_back(99);       // vectors of primary types on the other hand don't need to worry about deallocation
 }
